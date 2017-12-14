f75665e1eb61ed21ba6890714c8711f40ac16fb2
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-12-14 13:00:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 //has many trips
 //has many passengers though trips
-let driverId = 0
-let passengerId = 0
-let tripId = 0
-let store = {drivers: [], passengers: [], trips: []}
+let store = { drivers: [], passengers: [], trips: [] };
 
+let driverId = 0;
+let passengerId = 0;
+let tripId = 0;
 class Driver {
   constructor(name) {
     this.name = name;
